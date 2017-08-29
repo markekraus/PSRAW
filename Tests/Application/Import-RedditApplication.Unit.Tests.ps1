2fb18ee0f7ab4ecd24f6815c4bedc8252e224e03
markekraus <github@markekraus.com>
2017-08-29 04:33:11 -0500

    [ci skip] Rework Import-RedditApplication

--- Tests/Application/Import-RedditApplication.Unit.Tests.ps1
+++ Tests/Application/Import-RedditApplication.Unit.Tests.ps1
@@ -11,85 +11,39 @@
     .DESCRIPTION
         Import-RedditApplication Function unit tests
 #>
-
-$ProjectRoot = Resolve-Path "$PSScriptRoot\..\.."
-$ModuleRoot = Split-Path (Resolve-Path "$ProjectRoot\*\*.psd1")
-$ModuleName = Split-Path $ModuleRoot -Leaf
-Remove-Module -Force $ModuleName  -ErrorAction SilentlyContinue
-Import-Module (Join-Path $ModuleRoot "$ModuleName.psd1") -force
-
-$Command = 'Import-RedditApplication'
-$TypeName = 'RedditApplication'
-
-$ClientId = '54321'
-$ClientSecret = '12345'
-$SecClientSecret = $ClientSecret | ConvertTo-SecureString -AsPlainText -Force 
-$ClientCredential = [pscredential]::new($ClientId, $SecClientSecret)
-
-$UserId = 'reddituser'
-$UserSecret = 'password12345'
-$SecUserSecret = $UserSecret | ConvertTo-SecureString -AsPlainText -Force 
-$UserCredential = [pscredential]::new($UserId, $SecUserSecret)
-
-$ExportFile = '{0}\RedditApplicationExport-{1}.xml' -f $env:TEMP, [guid]::NewGuid().toString()
-
-$Application = [RedditApplication]@{
-    Name             = 'TestApplication'
-    Description      = 'This is only a test'
-    RedirectUri      = 'https://localhost/'
-    UserAgent        = 'windows:PSRAW-Unit-Tests:v1.0.0.0'
-    Scope            = 'read'
-    ClientCredential = $ClientCredential
-    UserCredential   = $UserCredential
-    Type             = 'Script'
-    ExportPath       = $ExportFile 
-}
-
-$Application | Export-Clixml -Path $ExportFile 
-
-$ParameterSets = @(
-    @{
-        Name   = 'Path'
-        Params = @{
-            Path = $ExportFile
-        }
+Describe "Import-RedditApplication" -Tags Build, Unit {
+    BeforeAll{
+        Initialize-PSRAWTest
+        Remove-Module $ModuleName -Force -ErrorAction SilentlyContinue
+        Import-Module -force $ModulePath
+        $ExportFile = '{0}\RedditApplicationExport-{1}.xml' -f $TestDrive, [guid]::NewGuid().toString()
+        $Application = Get-ApplicationScript
+        $Application.ExportPath = $ExportFile
+        $Application | Export-Clixml -Path $ExportFile 
+        $TestCases = @(
+            @{
+                Name   = 'Path'
+                Params = @{
+                    Path = $ExportFile
+                }
+            }
+            @{
+                Name   = 'LiteralPath'
+                Params = @{
+                    LiteralPath = $ExportFile
+                }
+            }
+        )
     }
-    @{
-        Name   = 'LiteralPath'
-        Params = @{
-            LiteralPath = $ExportFile
-        }
+    It "'<Name>' Parameter set does not have errors" -TestCases $TestCases {
+        param($Name,$Params)
+        { Import-RedditApplication @Params -ErrorAction Stop } | Should not throw
     }
-)
-
-function MyTest {
-    foreach ($ParameterSet in $ParameterSets) {
-        It "'$($ParameterSet.Name)' Parameter set does not have errors" {
-            $LocalParams = $ParameterSet.Params
-            { & $Command @LocalParams -ErrorAction Stop } | Should not throw
-        }
+    It "Emits a 'RedditApplication' Object" {
+        (Get-Command Import-RedditApplication).OutputType.Name.where( { $_ -eq 'RedditApplication' }) | Should be 'RedditApplication'
     }
-    It "Emits a $TypeName Object" {
-        (Get-Command $Command).OutputType.Name.where( { $_ -eq $TypeName }) | Should be $TypeName
-    }
-    It "Returns a $TypeName Object" {
-        $LocalParams = $ParameterSets[0].Params.psobject.Copy()
-        $Object = & $Command @LocalParams | Select-Object -First 1
-        $Object.psobject.typenames.where( { $_ -eq $TypeName }) | Should be $TypeName
+    It "Returns a 'RedditApplication' Object" {
+        $Object = Import-RedditApplication -Path $ExportFile | Select-Object -First 1
+        $Object.psobject.typenames.where( { $_ -eq 'RedditApplication' }) | Should be 'RedditApplication'
     }
 }
-
-Describe "$command Unit" -Tags Unit {
-    $CommandPresent = Get-Command -Name $Command -Module $ModuleName -ErrorAction SilentlyContinue
-    if (-not $CommandPresent) {
-        Write-Warning "'$command' was not found in '$ModuleName' during pre-build tests. It may not yet have been added the module. Unit tests will be skipped until after build."
-        return
-    }
-    MyTest
-}
-
-Describe "$command Build" -Tags Build {
-    MyTest
-}
-
-Remove-Item -Force -Path $ExportFile -ErrorAction SilentlyContinue
