fcac3408bea6917e8dd4a6d5e67f680fb32324c7
markekraus <github@markekraus.com>
2017-08-31 04:03:35 -0500

    [ci skip] Rework RedditDate Tests

--- Tests/Classes/006-RedditDate.Unit.Tests.ps1
+++ Tests/Classes/006-RedditDate.Unit.Tests.ps1
@@ -1,25 +1,23 @@
-<#	
+<#
     .NOTES
-    
+
      Created with:  VSCode
      Created on:    6/02/2017 4:28 AM
-     Edited on:     5/29/2017
+     Edited on:     8/30/2017
      Created by:    Mark Kraus
-     Organization: 	
+     Organization:
      Filename:     RedditDate.Unit.Tests.ps1
-    
+
     .DESCRIPTION
         Unit Tests for RedditDate Class
 #>
-$ProjectRoot = Resolve-Path "$PSScriptRoot\..\.."
-$ModuleRoot = Split-Path (Resolve-Path "$ProjectRoot\*\*.psd1")
-$ModuleName = Split-Path $ModuleRoot -Leaf
-Remove-Module -Force $ModuleName  -ErrorAction SilentlyContinue
-Import-Module (Join-Path $ModuleRoot "$ModuleName.psd1") -force
 
-$Class = 'RedditDate'
-
-Function MyTest {
+Describe "[RedditDate] Build Tests" -Tag Build {
+    BeforeAll {
+        Initialize-PSRAWTest
+        Remove-Module $ModuleName -Force -ErrorAction SilentlyContinue
+        Import-Module -force $ModulePath
+    }
     It 'Creates a RedditDate from a Unix string' {
         $RedditDate = [RedditDate]'31536000'
         $RedditDate.Date | should be (get-date '1971/1/1')
@@ -31,8 +29,12 @@ Function MyTest {
         $RedditDate.Unix | should be 31536000
     }
     It 'Throws when the string cannot be parsed' {
-        Try { [RedditDate]'invalid' } 
-        Catch { $Exception = $_ } 
+        Try {
+            [RedditDate]'invalid'
+        }
+        Catch {
+            $Exception = $_
+        }
         $Message = 'Cannot convert value "invalid" to type "RedditDate". Error: "Unable to parse string as System.DateTime or System.Double."'
         $Exception.Exception.Message | Should Be $Message
         $Exception.Exception.InnerException.InnerException.InnerException | should BeOfType 'System.ArgumentException'
@@ -45,15 +47,4 @@ Function MyTest {
         $RedditDate = [RedditDate](get-date '1971/1/1')
         $RedditDate.Unix | Should be 31536000.0
     }
-}
-
-Describe "[$Class] Unit Tests" -Tag Unit {
-    if (-not ($Class -as [Type])) {
-        Write-Warning "'$class' was not found in '$ModuleName' during pre-build tests. It may not yet have been added the module. Unit tests will be skipped until after build."
-        return
-    }
-    MyTest
-}
-Describe "[$Class] Build Tests" -Tag Build {
-    MyTest
 }
