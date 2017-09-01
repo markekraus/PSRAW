559ff61b0156dc7be65a074eb8d3fd2fdfcefcbe
markekraus <github@markekraus.com>
2017-09-01 04:35:32 -0500

    [ci skip] Rework RedditModReport Tests

--- Tests/Classes/008-RedditModReport.Unit.Tests.ps1
+++ Tests/Classes/008-RedditModReport.Unit.Tests.ps1
@@ -1,39 +1,45 @@
-<#	
+<#
     .NOTES
-    
+
      Created with:  VSCode
      Created on:    6/02/2017 4:28 AM
-     Edited on:     6/02/2017
+     Edited on:     9/01/2017
      Created by:    Mark Kraus
-     Organization: 	
-     Filename:     RedditModReport.Unit.Tests.ps1
-    
+     Organization:
+     Filename:     008-RedditModReport.Unit.Tests.ps1
+
     .DESCRIPTION
         Unit Tests for RedditModReport Class
 #>
-$ProjectRoot = Resolve-Path "$PSScriptRoot\..\.."
-$ModuleRoot = Split-Path (Resolve-Path "$ProjectRoot\*\*.psd1")
-$ModuleName = Split-Path $ModuleRoot -Leaf
-Remove-Module -Force $ModuleName  -ErrorAction SilentlyContinue
-Import-Module (Join-Path $ModuleRoot "$ModuleName.psd1") -force
 
-$Class = 'RedditModReport'
+Describe "[RedditModReport] Build Tests" -Tag Build, Unit {
+    BeforeAll {
+        Initialize-PSRAWTest
+        Remove-Module $ModuleName -Force -ErrorAction SilentlyContinue
+        Import-Module -force $ModulePath
 
-Function MyTest {
-    It 'Creates a RedditModReport from an object collection' {
-        $RedditModReport = [RedditModReport]@('Breaks Rule 12', 'markekraus')
-        $RedditModReport.Reason | should be 'Breaks Rule 12'
-        $RedditModReport.Moderator | Should be 'markekraus'
+        $TestCases = @(
+            @{
+                Name = 'TestHash'
+                Hash = @{
+                    Reason    = 'Spam'
+                    Moderator = 'TestMod'
+                }
+            }
+        )
     }
-}
-
-Describe "[$Class] Unit Tests" -Tag Unit {
-    if (-not ($Class -as [Type])) {
-        Write-Warning "'$class' was not found in '$ModuleName' during pre-build tests. It may not yet have been added the module. Unit tests will be skipped until after build."
-        return
+    It "Converts the '<Name>' hash" -TestCases $TestCases {
+        param($Name, $Hash)
+        { [RedditModReport]$Hash } | should not throw
     }
-    MyTest
-}
-Describe "[$Class] Build Tests" -Tag Build {
-    MyTest
+    It "Has a working default constructor" {
+        { [RedditModReport]::New() } | Should not throw
+    }
+    It "Has a working ([Object[]]`$InputObjects) constructor" {
+        $Result = @{}
+        $InputObject = @('Breaks Rule 12', 'markekraus')
+        { $Result['object'] = [RedditModReport]::New($InputObject) } | Should not throw
+        $Result['object'].Reason    | Should Be 'Breaks Rule 12'
+        $Result['object'].Moderator | Should Be 'markekraus'
+    }
 }
