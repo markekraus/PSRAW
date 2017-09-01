2cbe36f0ad657cd24f6fe40eb1f6e4993a4a3ff6
markekraus <github@markekraus.com>
2017-09-01 04:37:10 -0500

    [ci skip] Rework RedditUserReport Tests

--- PSRAW/Classes/008-RedditUserReport.ps1
+++ PSRAW/Classes/008-RedditUserReport.ps1
@@ -1,19 +1,21 @@
-<#	
+<#
     .NOTES
-    
+
      Created with: 	Plaster
      Created on:   	6/1/2017 1:47 PM
      Edited on:     6/1/2017
      Created by:   	Mark Kraus
-     Organization: 	 
+     Organization:
      Filename:     	008-RedditUserReport.ps1
-    
+
     .DESCRIPTION
         RedditUserReport Class
 #>
 Class RedditUserReport {
     [String]$Reason
     [long]$Count
+    RedditUserReport () {
+    }
     RedditUserReport ([Object[]]$InputObjects) {
         $This.Reason = $InputObjects[0]
         $This.Count = $InputObjects[1]
